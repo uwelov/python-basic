bc841cd40f3864d7b0ee5a646333d96acd1e326c
Alisa <alisabeketova@gmail.com>
2026-07-23 21:35:06 +0300

    homework13

--- lessons 13/hw13.1.py
+++ lessons 13/hw13.1.py
@@ -4,15 +4,12 @@ import re
 
 def delete_html_tags(html_file, result_file='cleaned.txt'):
     with codecs.open(html_file, 'r', 'utf-8') as file:
-        result = []
+        html = file.read()
 
-        for line in file:
-            current_text_list = re.findall(r">([^<>]+)<", line)
-            if len(current_text_list) > 0:
-                result += current_text_list
+    cleaned_text = re.sub(r'<[^>]*>', '', html)
 
-    with codecs.open(result_file, 'w', 'utf-8') as new_file:
-        new_file.write('\n'.join(result))
+    with codecs.open(result_file, 'w', 'utf-8') as file:
+        file.write(cleaned_text)
 
 
 delete_html_tags('draft.html')
